88b840f5e5a105b8320a7d425d775cac0472e7fb
mayurkanade44 <mayurkanade44@gmail.com>
2023-03-20 18:04:25 +0530

    stats page with all client details added

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,4 +1,57 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getCompanyServices } from "../redux/adminSlice";
+import { toggleSidebar } from "../redux/userSlice";
+
 const Stats = () => {
-  return <div>Stats</div>;
+  const { adminLoading, allClients } = useSelector((store) => store.admin);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCompanyServices());
+  }, []);
+
+  return (
+    <div className="add-client">
+      <div className="row">
+        <h4 className="text-center">All Clients</h4>
+        {allClients?.map((item, index) => {
+          return (
+            <div className="col-md-4" key={item._id}>
+              <div
+                className={`card text-bg-light text-center ${
+                  index % 2 === 0 ? "border-success" : "border-warning"
+                }`}
+              >
+                <div className="card-header">{item.shipToName}</div>
+                <div className="card-body py-2">
+                  <h6 className="card-subtitle mb-2 text-muted">
+                    {item.shipToAddress}
+                  </h6>
+                  <p className="card-text mb-0">
+                    Some quick example text to build on the card title and make
+                  </p>
+                  <Link
+                    to={`/dashboard/client/${item._id}`}
+                    className="btn btn-sm btn-primary"
+                    onClick={() => dispatch(toggleSidebar())}
+                  >
+                    All Sub Locations
+                  </Link>
+                  <Link
+                    to={`/dashboard/client/${item._id}`}
+                    className="btn btn-sm ms-2 "
+                  >
+                    Edit Details
+                  </Link>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Stats;
